f1c0b2267f1b83d72f718e04ccd1d792ee704e07
Lakshitha Matarage <lnalindanath@gmail.com>
2021-09-21 21:24:44 +0530

    created hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Now Time is : " + LocalDateTime.now().toString());
+    }
+}
+
